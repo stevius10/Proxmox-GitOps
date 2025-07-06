a3cf7c5fbd2b0cae0b9388d814dfc496f9321f03
steven <steven.johann.koch@googlemail.com>
2025-07-06 16:58:44 +0200

    optional attributes for local development

---
+++ libs/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+COOKBOOK="$1"
+PROJECT_DIR="$PWD"
+
+DOCKER_IMAGE_NAME="proxmoxgitops"
+DOCKER_CONTAINER_NAME="${COOKBOOK}"
+DOCKER_INIT_WAIT="${DOCKER_INIT_WAIT:-5}"
+
+CONTAINER_ID=$(docker ps -aq --filter "name=$DOCKER_CONTAINER_NAME")
+if [[ -z "$CONTAINER_ID" ]]; then
+  CONTAINER_ID=$(docker run -d --privileged \
+    --tmpfs /share \
+    -w "/tmp" \
+    -v "$PROJECT_DIR:/tmp" \
+    -v "$PROJECT_DIR/../config/libraries/":"/tmp/$COOKBOOK/libraries/" \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:rw \
+    --cgroupns=host \
+    --name "$DOCKER_CONTAINER_NAME" \
+    "$DOCKER_IMAGE_NAME")
+  sleep "$DOCKER_INIT_WAIT"
+fi
+
+docker exec "$CONTAINER_ID" cinc-client -l info --local-mode --config-option cookbook_path=/tmp --chef-license accept --config-option node_path=/tmp $( [ -f /tmp/$COOKBOOK/config.json ] && echo "-j /tmp/$COOKBOOK/config.json" ) -o "$COOKBOOK"

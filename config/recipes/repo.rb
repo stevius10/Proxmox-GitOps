4dcb4a70554e10302839b0b49a8d235e80d695ae
steven <steven.johann.koch@googlemail.com>
2025-07-11 15:00:32 +0200

    recursive monorepo for self-referenced rolling release

--- config/recipes/repo.rb
+++ config/recipes/repo.rb
@@ -1,164 +1,203 @@
-file ".gitmodules" do
-  content ''
-  action :create
+require 'find'
+require 'fileutils'
+
+path_source = ENV['PWD']
+path_destination = node['git']['workspace']
+path_home = "/home/#{node['git']['app']['user']}"
+
+execute 'git_global_config' do
+  command <<-EOH
+    git config --global user.name "#{Env.get(node, 'login')}"
+    git config --global user.email "#{Env.get(node, 'email')}"
+  EOH
+  user node['git']['app']['user']
+  environment 'HOME' => path_home
 end
 
-node['git']['repositories'].each do |repo_name|
-  src = (repo_name == "./") ? ENV['PWD'] : File.expand_path(repo_name, ENV['PWD'])
-  name = File.basename(src)
-  dst = File.join(node['git']['workspace'], name)
+(node['git']['repositories'].sort_by { |r| r == "./" ? 1 : 0 }).each do |repo_path_relative|
+  path_source_repo = (repo_path_relative == "./") ? path_source : File.expand_path(repo_path_relative, path_source)
+  name_repo = File.basename(path_source_repo)
+  path_destination_repo = (repo_path_relative == "./") ? path_destination : File.expand_path(name_repo, path_destination)
 
-  directory dst do
+  directory path_destination_repo do
     owner     node['git']['app']['user']
     group     node['git']['app']['group']
     mode      '0755'
+    recursive true
     action    :create
   end
 
-  ruby_block "git_repo_#{name}" do
+  ruby_block "git_repo_#{name_repo}" do
     block do
       require 'net/http'
       require 'uri'
       require 'json'
 
-      check_api = URI("#{node['git']['endpoint']}/repos/#{node['git']['repo']['org']}/#{name}")
-      check_req = Net::HTTP::Get.new(check_api.request_uri)
-      check_req.basic_auth(Env.get(node, 'login'), Env.get(node, 'password'))
-      check_response = Net::HTTP.new(check_api.host, check_api.port).request(check_req)
+      api = URI("#{node['git']['endpoint']}/repos/#{node['git']['repo']['org']}/#{name_repo}")
+      req = Net::HTTP::Get.new(api.request_uri)
+      req.basic_auth(Env.get(node, 'login'), Env.get(node, 'password'))
+      response = Net::HTTP.new(api.host, api.port).request(req)
 
-      if check_response.code.to_i == 404
+      if response.code.to_i == 404
         api = URI("#{node['git']['endpoint']}/admin/users/#{node['git']['repo']['org']}/repos")
         http = Net::HTTP.new(api.host, api.port)
         req = Net::HTTP::Post.new(api.request_uri)
         req.basic_auth(Env.get(node, 'login'), Env.get(node, 'password'))
         req['Content-Type'] = 'application/json'
-        req.body = { name: name, private: false, auto_init: false, default_branch: node['git']['repo']['branch'] }.to_json
+        req.body = { name: name_repo, private: false, auto_init: false, default_branch: 'main' }.to_json
 
         code = http.request(req).code.to_i
-        node.run_state["#{name}_repo_created"] = (code == 201)
-        node.run_state["#{name}_repo_exists"] = false
-        raise "#{name} (HTTP #{code}): #{response.body}" unless [201, 409].include?(code)
+        node.run_state["#{name_repo}_repo_created"] = (code == 201)
+        node.run_state["#{name_repo}_repo_exists"] = false
+        raise "#{name_repo} (HTTP #{code}): #{response.body}" unless [201, 409].include?(code)
       else
-        node.run_state["#{name}_repo_created"] = false
-        node.run_state["#{name}_repo_exists"] = true
+        node.run_state["#{name_repo}_repo_created"] = false
+        node.run_state["#{name_repo}_repo_exists"] = true
       end
     end
     action :run
   end
 
-  execute "git_config_#{name}" do
-    command <<-EOH
-      git config --global user.name "#{Env.get(node, 'login')}"
-      git config --global user.email "#{Env.get(node, 'email')}"
-    EOH
-    action :run
-  end
-
-  execute "git_init_#{name}" do
-    command "git init -b #{node['git']['repo']['branch']}"
-    cwd dst
+  execute "git_init_#{name_repo}" do
+    command "git init -b main"
+    cwd path_source_repo
     user node['git']['app']['user']
     action :run
-    only_if { !::Dir.exist?("#{dst}/.git") }
+    only_if { ::Dir.exist?("#{path_destination_repo}/.git") }
   end
 
-  template "#{dst}/.git/config" do
+  template "#{path_source_repo}/.git/config" do
     source 'repo_config.erb'
     owner node['git']['app']['user']
     group node['git']['app']['group']
     mode '0644'
-    variables(repo: name, git_user: node['git']['app']['user'])
+    variables(repo: name_repo, git_user: node['git']['app']['user'])
     action :create
-    only_if { ::Dir.exist?("#{dst}/.git") }
+    only_if { ::Dir.exist?("#{path_source_repo}/.git") }
   end
 
-  execute "git_prepare_#{name}" do
+  execute "git_overwrite_#{name_repo}" do
+    cwd path_source_repo
+    user node['git']['app']['user']
+    environment 'HOME' => path_home
     command <<-EOH
-      git -c user.name="#{Env.get(node, 'login')}" -c user.email="#{Env.get(node, 'email')}" commit --allow-empty -m '[skip ci]' -m 'initial commit [skip ci]' && git push -u origin HEAD:main && git push -f origin HEAD:release
+      git checkout -b #{name_repo}/snapshot && git add -A && git commit -am "last running configuration [skip ci]" || true
+      git fetch --all && git checkout main && git reset --hard origin/main && git branch -f release origin/main
+      git cherry-pick snapshot
     EOH
-    cwd dst
-    user node['git']['app']['user']
-    environment 'HOME' => "/home/#{node['git']['app']['user']}"
     action :run
-    only_if { node.run_state["#{name}_repo_created"] }
+    only_if { ::Dir.exist?("#{path_destination_repo}/.git") && node.run_state["#{name_repo}_repo_exists"] }
   end
 
-  execute "git_pull_#{name}" do
-    command "git -c user.name='#{Env.get(node, 'login')}' -c user.email='#{Env.get(node, 'email')}' pull"
-    cwd dst
-    user node['git']['app']['user']
-    environment 'HOME' => "/home/#{node['git']['app']['user']}"
-    action :run
-    only_if { node.run_state["#{name}_repo_exists"] }
-  end
-
-  if repo_name == "./"
-    submodules = node['git']['repositories'].reject { |r| r == "./" }
-
-    submodules.each do |path|
-      sub_name = File.basename(File.expand_path(path, ENV['PWD']))
-      repo_url = "#{node['git']['endpoint'].split('/api/v1').first}/#{node['git']['repo']['org']}/#{sub_name}.git"
-
-      execute "git_submodule_#{sub_name}" do
-        cwd dst
-        user node['git']['app']['user']
-        environment 'HOME' => "/home/#{node['git']['app']['user']}"
-        command <<-EOH
-          if ! git config --file .gitmodules --get-regexp path | grep -q "^submodule\\.#{sub_name}\\.path"; then
-            git submodule add #{repo_url} #{path}
-          fi
-        EOH
-        only_if { ::Dir.exist?("#{dst}/.git") }
-      end
-    end
-
-    execute "git_submodules_#{name}" do
-      command "git submodule update --init --recursive"
-      cwd dst
-      user node['git']['app']['user']
-      environment 'HOME' => "/home/#{node['git']['app']['user']}"
-      action :run
-      only_if { ::Dir.exist?("#{dst}/.git") }
-    end
-  end
-
-  # bei ./ muss in dir config gewechselt werden
-  ruby_block "git_desired_state_#{name}" do
+  ruby_block 'create_workspace' do
     block do
       require 'fileutils'
-      Dir.children(src).each do |entry|
-        next if entry == '.git'
-        src_entry = File.join(src, entry)
-        dst_entry = File.join(dst, entry)
-        if File.directory?(src_entry)
-          FileUtils.cp_r("#{src_entry}/.", dst_entry, remove_destination: true, verbose: true) unless entry == '.git'
+      Find.find(path_source_repo) do |path_source_repository|
+        next if path_source_repository =~ /(^|\/)\.git(\/|$)/ || File.basename(path_source_repository) == '.gitmodules'
+        path_source_repository_relative = path_source_repository.sub(/^#{Regexp.escape(path_source_repo)}\/?/, '')
+        path_destination_repository = File.join(path_destination_repo, path_source_repository_relative)
+        if File.directory?(path_source_repository)
+          FileUtils.mkdir_p(path_destination_repository)
         else
-          FileUtils.cp(src_entry, dst_entry, verbose: true)
+          FileUtils.mkdir_p(File.dirname(path_destination_repository))
+          FileUtils.cp(path_source_repository, path_destination_repository, verbose: true)
         end
       end
+      FileUtils.chown_R(node['git']['app']['user'], node['git']['app']['group'], path_destination_repo)
     end
     action :run
   end
 
-  execute "git_push_#{name}" do
-    cwd dst
+  execute "git_init_#{name_repo}" do
+    command "git init -b main"
+    cwd path_destination_repo
     user node['git']['app']['user']
-    environment 'HOME' => "/home/#{node['git']['app']['user']}"
-    command <<-EOH
-      git add --all
-      if ! git diff --quiet || ! git diff --cached --quiet; then
-        git commit --allow-empty -m "[skip ci]"
-        git push -f origin HEAD:main
-        sleep 3
-        if ! git ls-remote origin refs/for/release | grep -q "$(git rev-parse HEAD)"; then
-          git push origin HEAD:refs/for/release \
-            -o topic="release" \
-            -o title="Release Pull Request" \
-            -o description="Created automatically for deployment." \
-            -o force-push
-        fi
-      fi
-    EOH
     action :run
   end
+
+  template "#{path_destination_repo}/.git/config" do
+    source 'repo_config.erb'
+    owner node['git']['app']['user']
+    group node['git']['app']['group']
+    mode '0644'
+    variables(repo: name_repo, git_user: node['git']['app']['user'])
+    action :create
+  end
+
+  execute "git_create_#{name_repo}" do
+    command <<-EOH
+      git commit --allow-empty -m 'initial commit [skip ci]' && git push -u origin HEAD:main &&
+      (git ls-remote --exit-code origin release >/dev/null 2>&1 && git push -f origin HEAD:release || git push -u origin HEAD:release)
+    EOH
+    cwd path_destination_repo
+    user node['git']['app']['user']
+    environment 'HOME' => path_home
+    action :run
+    only_if { node.run_state["#{name_repo}_repo_created"]  }
+  end
+
+  if repo_path_relative == "./"
+    submodules = node['git']['repositories'].reject { |r| r == "./" }
+
+    ruby_block 'set_gitmodules' do
+      block do
+        path_destination_gitmodules = File.join(path_destination, '.gitmodules')
+        if File.exist?(path_destination_gitmodules)
+          gitmodules = File.read(path_destination_gitmodules).gsub(/(url\s*=\s*http:\/\/)([^:\/\s]+)/) do
+            "#{$1}#{node['ip']}"
+          end
+          File.write(path_destination_gitmodules, gitmodules)
+        end
+      end
+      action :run
+    end
+
+    submodules.each do |submodule_path_relative|
+      module_path_relative = submodule_path_relative.sub(%r{^\./}, '')
+      module_name = File.basename(module_path_relative)
+      submodule_url = "#{node['git']['endpoint'].split('/api/v1').first}/#{node['git']['repo']['org']}/#{module_name}.git"
+
+      directory File.join(path_destination_repo, module_path_relative) do
+        recursive true
+        action :delete
+      end
+
+      execute "git_submodule_#{module_name}" do
+        cwd path_destination_repo
+        user node['git']['app']['user']
+        environment 'HOME' => path_home
+        command <<-EOH
+          if ! git config --file .gitmodules --get-regexp path | grep -q "^submodule\\.#{module_name}\\.path"; then
+            git submodule add #{submodule_url} #{module_path_relative}
+          fi
+          # git config submodule.#{module_name}.url #{submodule_url} || true
+        EOH
+        only_if { ::Dir.exist?("#{path_destination_repo}/.git") }
+      end
+    end
+  end
+
+  execute "git_push_#{name_repo}" do
+    cwd path_destination_repo
+    user node['git']['app']['user']
+    environment 'HOME' => path_home
+    command <<-EOH
+    git add --all
+    if ! git diff --quiet || ! git diff --cached --quiet; then
+      git commit --allow-empty -m "[skip ci]"
+      git push -f origin HEAD:main
+      sleep 3
+      if ! git ls-remote origin refs/for/release | grep -q "$(git rev-parse HEAD)"; then
+        git push origin HEAD:refs/for/release \
+          -o topic="release" \
+          -o title="Release Pull Request" \
+          -o description="Created automatically for deployment." \
+          -o force-push
+      fi
+    fi
+    rm -rf #{path_destination_repo}
+  EOH
+    action :run
+  end
+
 end

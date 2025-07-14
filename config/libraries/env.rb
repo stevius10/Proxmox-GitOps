00dd45ade9129f02f7d05cd6e429fb9cc832ac09
steven <steven.johann.koch@googlemail.com>
2025-07-14 21:04:40 +0200

    fail resistence

--- config/libraries/env.rb
+++ config/libraries/env.rb
@@ -4,36 +4,27 @@ require 'json'
 require 'active_support/core_ext/object/blank'
 
 module Env
-  class AuthError < StandardError; end
-  class RequestError < StandardError; end
-
+  
   def self.creds(node, login_key = 'login', password_key = 'password')
-    Chef::Log.info(login = ENV[login_key.upcase] || node[login_key.to_sym])
-    Chef::Log.info(pass = ENV[password_key.upcase] || node[password_key.to_sym])
-    raise AuthError, "Auth failed – node: #{node[login_key.to_sym].inspect}, env: #{ENV[login_key.upcase].inspect}" if login.blank? || pass.blank?
-    [login, pass]
+    [login = ENV[login_key.upcase] || node[login_key.to_sym], pass = ENV[password_key.upcase] || node[password_key.to_sym]].tap { Chef::Log.info(login); Chef::Log.info(pass) }
   end
 
   def self.get(node, key)
-    Chef::Log.info(result = node[key].to_s.presence || ENV[key.to_s.upcase].presence || get_variable(node, key))
-    result
-  rescue StandardError => e
-    Chef::Log.warn("#{e.message} – node: #{node[key].inspect}, env: #{ENV[key.to_s.upcase].inspect}")
-    raise
+    Chef::Log.info(val = node[key].to_s.presence || ENV[key.to_s.upcase].presence || get_variable(node, key)); val
+  rescue => e
+    Chef::Log.war"[#{__method__}] #{e.message} node[#{key}]: #{node[key].inspect} ENV[#{key}]: #{ENV[key.to_s.upcase].inspect}"
   end
 
   def self.get_variable(node, key)
-    resp = request(node, key)
-    raise RequestError, "Failed: Get '#{key}' from #{host(node)} – status #{resp.code}" unless resp.is_a?(Net::HTTPOK)
-    JSON.parse(resp.body)['data']
+    JSON.parse(request(node, key).body)['data']
+  rescue => e
+    Chef::Log.war"[#{__method__}] #{e.message} failed get '#{key}' on #{host(node)} node[#{key}]: #{node[key].inspect} ENV[#{key}]: #{ENV[key.to_s.upcase].inspect}"
   end
 
-  def self.set_variable(node, key, value)
-    resp = request(node, key, { name: key, value: value.to_s }.to_json)
-    raise RequestError, "Failed: Set '#{key}' on #{host(node)} – status #{resp.code}" unless %w[201 204 409 422].include?(resp.code)
-    true
-  rescue StandardError => e
-    Chef::Log.warn("#{e.message} – node: #{node[key].inspect}, env: #{ENV[key.to_s.upcase].inspect}")
+  def self.set_variable(node, key, val)
+    request(node, key, { name: key, value: val.to_s }.to_json)
+  rescue => e
+    Chef::Log.war"[#{__method__}] #{e.message} failed set '#{key}' on #{host(node)} node[#{key}]: #{node[key].inspect} ENV[#{key}]: #{ENV[key.to_s.upcase].inspect}"
     raise
   end
 
@@ -42,18 +33,17 @@ module Env
   end
 
   private_class_method def self.host(node)
-    Chef::Log.info(result = node['host'].to_s.presence || ENV['HOST'].presence || '127.0.0.1')
-    result
+    node['host'].to_s.presence || ENV['HOST'].presence || '127.0.0.1'
   end
 
   private_class_method def self.request(node, key, body = nil)
-    uri = URI.parse("http://#{host(node)}:8080/api/v1/orgs/srv/actions/variables/#{key}")
-    req = body ? Net::HTTP::Post.new(uri) : Net::HTTP::Get.new(uri)
+    uri = URI("http://#{host(node)}:8080/api/v1/orgs/srv/actions/variables/#{key}")
+    req = (body ? Net::HTTP::Post : Net::HTTP::Get).new(uri)
     req.basic_auth(*creds(node))
-    if body
-      req['Content-Type'] = 'application/json'
-      req.body = body
-    end
-    Net::HTTP.start(uri.host, uri.port) { |http| http.request(req) }
+    req['Content-Type'] = 'application/json' and req.body = body if body
+    Net::HTTP.start(uri.host, uri.port) { |h| h.request(req) }
+  rescue => e
+    Chef::Log.war"[#{__method__}] #{e.message} fail '#{key}' on #{host(node)} node[#{key}]: #{node[key].inspect} ENV[#{key}]: #{ENV[key.to_s.upcase].inspect} #{body}"
   end
+
 end

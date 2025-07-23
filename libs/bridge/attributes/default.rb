0e439aa97fb853c5a43ded5e844b349d90eb3109
steven <steven.johann.koch@googlemail.com>
2025-07-23 16:09:37 +0200

    fix typo in dir name

--- libs/bridge/attributes/default.rb
+++ libs/bridge/attributes/default.rb
@@ -5,6 +5,6 @@ default['bridge']['port']       = 8080
 default['bridge']['serial']     = Env.get(node, 'serial') || '/dev/serial/by-id/'
 
 default['bridge']['dir']        = '/app/bridge'
-default['bridge']['data']       = "#{node['bridge']['dir']}/data'"
+default['bridge']['data']       = "#{node['bridge']['dir']}/data"
 
 default['bridge']['version']    = 'latest'

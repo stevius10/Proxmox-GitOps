4982211e227199f890c620f3fa6e7c0e587299ae
steven <steven.johann.koch@googlemail.com>
2025-07-30 20:59:41 +0200

    idempotent systemd handling

--- config/recipes/runner.rb
+++ config/recipes/runner.rb
@@ -17,7 +17,7 @@ template "#{node['runner']['install_dir']}/config.yaml" do
 end
 
 Common.application(self, 'runner',
-  user: node['git']['app']['user'],
+  user: node['git']['app']['user'], action: [:enable],
   exec: "#{node['runner']['install_dir']}/act_runner daemon --config #{node['runner']['install_dir']}/config.yaml",
   cwd: node['runner']['install_dir'],
   subscribe: ["template[#{node['runner']['install_dir']}/config.yaml]", "remote_file[#{node['runner']['install_dir']}/act_runner]"]
@@ -25,40 +25,44 @@ Common.application(self, 'runner',
 
 ruby_block 'runner_register' do
   block do
-    # unless ::File.exist?(node['runner']['marker_file']) # stability over convention
-      require 'net/http'
-      connected = false
-      15.times do
-        res = Common.request(URI("http://localhost:#{node['git']['port']['http']}"))
-        (connected = true; break) if res.is_a?(Net::HTTPSuccess) || res.is_a?(Net::HTTPRedirection)
+    require 'net/http'
+    uri = URI("http://localhost:#{node['git']['port']['http']}")
+    connected = 15.times.any? do
+      begin
+        res = Common.request(uri)
+        res.is_a?(Net::HTTPSuccess) || res.is_a?(Net::HTTPRedirection)
+      rescue Errno::ECONNREFUSED, SocketError
+        false
+      ensure
         sleep 5
       end
-      raise "Gitea not responding" unless connected
+    end
+    raise 'Gitea not responding' unless connected
 
-      (token = Mixlib::ShellOut.new(
-        "#{node['git']['install_dir']}/gitea actions --config #{node['git']['install_dir']}/app.ini generate-runner-token",
-        user: node['git']['app']['user'],
-        environment: { 'HOME' => "/home/#{node['git']['app']['user']}" }
-      )).run_command
-      token.error!
-      token = token.stdout.strip
+    (token = Mixlib::ShellOut.new(
+      "#{node['git']['install_dir']}/gitea actions --config #{node['git']['install_dir']}/app.ini generate-runner-token",
+      user: node['git']['app']['user'],
+      environment: { 'HOME' => "/home/#{node['git']['app']['user']}" }
+    )).run_command
+    token.error!
+    token = token.stdout.strip
 
-      (register = Mixlib::ShellOut.new(
-        "#{node['runner']['install_dir']}/act_runner register " \
-          "--instance http://localhost:#{node['git']['port']['http']} " \
-          "--token #{token} " \
-          "--no-interactive " \
-          "--labels shell " \
-          "--config #{node['runner']['install_dir']}/config.yaml",
-        cwd: node['runner']['install_dir'],
-        user: node['git']['app']['user'],
-        environment: { 'HOME' => "/home/#{node['git']['app']['user']}" }
-      )).run_command
-      register.error!
+    (register = Mixlib::ShellOut.new(
+      "#{node['runner']['install_dir']}/act_runner register " \
+        "--instance http://localhost:#{node['git']['port']['http']} " \
+        "--token #{token} " \
+        "--no-interactive " \
+        "--labels shell " \
+        "--config #{node['runner']['install_dir']}/config.yaml",
+      cwd: node['runner']['install_dir'],
+      user: node['git']['app']['user'],
+      environment: { 'HOME' => "/home/#{node['git']['app']['user']}" }
+    )).run_command
+    register.error!
 
-      # File.write(node['runner']['marker_file'], Time.now.to_s)
-    # end
+    # File.write(node['runner']['marker_file'], Time.now.to_s)
   end
+  # not_if { ::File.exist?(node['runner']['marker_file']) } # stability over convention
 end
 
 Common.application(self, 'runner')

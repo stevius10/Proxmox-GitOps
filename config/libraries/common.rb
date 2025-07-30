10e939a39cda485407a20da8aa187b5e1ab9fb94
steven <steven.johann.koch@googlemail.com>
2025-07-30 18:03:00 +0200

    changed: accessibility

--- config/libraries/common.rb
+++ config/libraries/common.rb
@@ -1,6 +1,6 @@
 module Common
 
-  def packages(ctx, *pkgs, action: :install)
+  def self.packages(ctx, *pkgs, action: :install)
     Array(pkgs).flatten.each do |pkg|
       ctx.package pkg do
         action action
@@ -8,7 +8,7 @@ module Common
     end
   end
 
-  def directories(ctx, dirs, owner:, group:, mode: '0755', recursive: true, action: :create)
+  def self.directories(ctx, dirs, owner:, group:, mode: '0755', recursive: true, action: :create)
     Array(dirs).each do |dir|
       ctx.directory dir do
         owner owner
@@ -31,7 +31,7 @@ module Common
     end
   end
 
-  def download(ctx, path, url:, owner: 'root', group: 'root', mode: '0644', action: :create)
+  def self.download(ctx, path, url:, owner: 'root', group: 'root', mode: '0644', action: :create)
     src = url.respond_to?(:call) ? lazy { url.call } : url
     ctx.remote_file path do
       source src
@@ -42,7 +42,7 @@ module Common
     end
   end
 
-  def request(uri, user: nil, pass: nil, headers: {}, method: Net::HTTP::Get, body: nil)
+  def self.request(uri, user: nil, pass: nil, headers: {}, method: Net::HTTP::Get, body: nil)
     req  = method.new(URI(uri))
     req.basic_auth(user, pass) if user && pass
     req.body = body if body
@@ -50,7 +50,7 @@ module Common
     Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(req) }
   end
 
-  def daemon(ctx, name)
+  def self.daemon(ctx, name)
     ctx.find_resource!(:execute, name)
   rescue Chef::Exceptions::ResourceNotFound
     ctx.execute name do
@@ -59,7 +59,7 @@ module Common
     end
   end
 
-  def application(ctx, name, user: 'app', group: user, exec: nil, cwd: nil, unit: {}, action: [:enable, :start], restart: 'on-failure', subscribe: nil, reload: 'systemd_reload')
+  def self.application(ctx, name, user: 'app', group: user, exec: nil, cwd: nil, unit: {}, action: [:enable, :start], restart: 'on-failure', subscribe: nil, reload: 'systemd_reload')
     if exec || !unit.empty?
       daemon(ctx, reload)
 

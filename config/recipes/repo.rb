654e996b1f6287d6d0afea03a00288c3493832d9
steven <steven.johann.koch@googlemail.com>
2025-08-13 19:40:51 +0200

    logging workaround for runtime caption

--- config/recipes/repo.rb
+++ config/recipes/repo.rb
@@ -19,16 +19,14 @@ Common.directories(self, [destination, working], recreate: true,
   path_working = "#{working}/#{name_repo}"
   path_destination = monorepo ? destination : File.expand_path(name_repo, destination)
 
-  Logs.info("#{repository} (#{name_repo})")
-
   ruby_block "repo_exists_#{name_repo}" do
+    only_if { Logs.info?("#{repository} (#{name_repo})") }
     block do
       node.run_state["#{name_repo}_repo_exists"] =
         (Utils.request("#{node['git']['api']['endpoint']}/repos/#{node['git']['org']['main']}/#{name_repo}",
           user: Env.get(node, 'login'), pass: Env.get(node, 'password'))
         ).code.to_i != 404
     end
-    action :run
   end
 
   execute "repo_exists_snapshot_create_#{name_repo}" do
@@ -53,12 +51,11 @@ Common.directories(self, [destination, working], recreate: true,
         Logs.request!("Failed to delete #{name_repo}", uri, response)
       end
     end
-    action :run
     only_if { node.run_state["#{name_repo}_repo_exists"] }
   end
 
-  Logs.info("[#{repository} (#{name_repo})]: request repository")
   ruby_block "repo_request_#{name_repo}" do
+    only_if { Logs.info?("[#{repository} (#{name_repo})] create repository") }
     block do
       require 'json'
       (response = Utils.request(
@@ -68,7 +65,6 @@ Common.directories(self, [destination, working], recreate: true,
         body: { name: name_repo, private: false, auto_init: false, default_branch: 'main' }.to_json
       )).code.to_i == 201 or Logs.request!("Error creating repository '#{name_repo}'", uri, response)
     end
-    action :run
   end
 
   execute "repo_git_init_#{name_repo}" do
@@ -84,12 +80,11 @@ Common.directories(self, [destination, working], recreate: true,
     group node['git']['user']['group']
     mode '0644'
     variables(repo: name_repo, git_user: node['git']['user']['app'] )
-    action :create
     only_if { ::File.directory?("#{path_destination}/.git") }
   end
 
-  Logs.info("[#{repository} (#{name_repo})]: base commit")
   execute "repo_git_empty_#{name_repo}" do
+    only_if { Logs.info?("[#{repository} (#{name_repo})] base commit") }
     command <<-EOH
       git commit --allow-empty -m "base commit [skip ci]" && git checkout -b release
       git push -u origin main && git push -u origin release
@@ -126,11 +121,9 @@ Common.directories(self, [destination, working], recreate: true,
       end
       FileUtils.chown_R(node['git']['user']['app'] , node['git']['user']['group'], path_destination)
     end
-    action :run
   end
 
   directory "#{path_destination}/.gitea/workflows" do
-    action :create
     recursive true
   end
 
@@ -139,7 +132,6 @@ Common.directories(self, [destination, working], recreate: true,
     owner node['git']['user']['app'] 
     group node['git']['user']['group']
     mode '0644'
-    action :create
     not_if { monorepo }
     not_if { File.exist?("#{path_destination}/.gitea/workflows/sync.yml") }
   end
@@ -149,16 +141,16 @@ Common.directories(self, [destination, working], recreate: true,
     owner node['git']['user']['app'] 
     group node['git']['user']['group']
     mode '0644'
-    action :create
     only_if { repository.include?('libs/') and File.exist?("#{path_destination}/config.env") }
     not_if { File.exist?("#{path_destination}/.gitea/workflows/pipeline.yml") }
   end
 
   if monorepo
     submodules = repositories.reject { |r| r == "./" } # without itself
-    Logs.info("#{repository} (monorepository): referencing #{submodules}")
+
 
     ruby_block 'repo_mono_submodule_rewritten' do
+      only_if { Logs.info?("#{repository} (monorepository): referencing #{submodules}") }
       block do
         path_dst_gitmodules = File.join(destination, '.gitmodules')
         if File.exist?(path_dst_gitmodules)
@@ -167,8 +159,7 @@ Common.directories(self, [destination, working], recreate: true,
               .gsub(/(url\s*=\s*http:\/\/)([^:\/\s]+)/) { "#{$1}#{node['host']}" })
         end
       end
-      action :run
-    end
+      end
 
     # Submodule handling in Monorepository
     submodules.each do |submodule|
@@ -183,11 +174,8 @@ Common.directories(self, [destination, working], recreate: true,
         action :delete
       end
 
-      Logs.info("#{repository} (monorepository): referencing #{path_module} (#{module_name})")
-
       execute "repo_mono_submodule_references_#{module_name}" do
-        cwd path_destination
-        user node['git']['user']['app'] 
+        only_if { Logs.info?("#{repository} (monorepository): referencing #{path_module} (#{module_name})") }
         command <<-EOH
           if ! git config --file .gitmodules --get-regexp path | grep -q "^submodule\\.#{module_name}\\.path"; then
             echo "submodule add: #{module_url} -> #{path_module}"
@@ -199,6 +187,8 @@ Common.directories(self, [destination, working], recreate: true,
             git add -f local/config.json
           fi
         EOH
+        cwd path_destination
+        user node['git']['user']['app']
       end
     end
 
@@ -227,7 +217,6 @@ Common.directories(self, [destination, working], recreate: true,
       fi
     fi
     EOH
-    action :run
   end
 
   directory path_destination do
@@ -247,7 +236,6 @@ Common.directories(self, [destination, working], recreate: true,
         Logs.request!("Failed to clean test/#{name_repo} (#{status_code})", uri, response) unless [204, 404].include?(status_code)
       end
     end
-    action :run
   end
 
   ruby_block "repo_stage_fork_create_#{name_repo}" do
@@ -259,7 +247,6 @@ Common.directories(self, [destination, working], recreate: true,
       ).code.to_i
       Logs.request!("Forking to #{node['git']['org']['stage']}/#{name_repo} failed", uri, response) unless [201, 202].include?(status_code)
     end
-    action :run
   end
 
 end

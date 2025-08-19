a99c5d363ca19444ef5982901d34f0367e71cd82
steven <steven.johann.koch@googlemail.com>
2025-08-19 19:03:40 +0200

    put environment block gathered to beginning

--- libs/broker/recipes/default.rb
+++ libs/broker/recipes/default.rb
@@ -1,8 +1,11 @@
 Env.dump(self, cookbook_name, repo: cookbook_name)
 
-Common.directories(self, [node['broker']['dir']['data'], node['broker']['dir']['log']])
-
 Env.set(self, 'broker', "mqtt://#{node['ip']}:#{node['broker']['port']}")
+
+login = Env.get(self, 'login')
+password = Env.get(self, 'password')
+
+Common.directories(self, [node['broker']['dir']['data'], node['broker']['dir']['log']])
 
 package 'mosquitto'
 
@@ -22,12 +25,13 @@ file node['broker']['file']['user'] do
   mode '0640'
 end
 
-execute "user_add_#{Env.get(self, 'login')}" do
-  command "mosquitto_passwd -b #{node['broker']['file']['user']} '#{Env.get(self, 'login')}' '#{Env.get(self, 'password')}'"
+execute "mosquitto_user_#{login}" do
+  command "mosquitto_passwd -b #{node['broker']['file']['user']} '#{login}' '#{password}'"
   user 'root'
   sensitive true
 end
 
 Common.application(self, 'mosquitto',
   exec:  "/usr/sbin/mosquitto -c #{node['broker']['file']['config']}",
-  subscribe: "template[#{node['broker']['file']['config']}]" )
+  subscribe: [ "template[#{node['broker']['file']['config']}]",
+               "template[#{node['broker']['file']['user']}]"  ] )

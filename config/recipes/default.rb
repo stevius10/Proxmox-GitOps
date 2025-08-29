285600b3b903b7f55ab6465737740c1be93f413c
stevius10 <steven.johann.koch@googlemail.com>
2025-08-29 18:23:44 +0200

    include tasks

--- config/recipes/default.rb
+++ config/recipes/default.rb
@@ -2,6 +2,6 @@ include_recipe 'config::prepare'
 include_recipe 'config::git'
 include_recipe 'config::runner'
 include_recipe 'config::config'
-# include_recipe 'config::repo'
+include_recipe 'config::repo'
 include_recipe 'config::task'
 include_recipe('config::customize') if node['git']['conf']['customize']

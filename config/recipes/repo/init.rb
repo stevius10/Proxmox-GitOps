af665df3429a979b50b7ac73bfd411c2b20df088
Steven <106664555+stevius10@users.noreply.github.com>
2025-08-30 18:49:45 +0200

    refine/modularization (#101)
    * revert splitting changes due changed distribution
    
    * move repo level variable dump to repo initialization for modular repeatability

--- config/recipes/repo/init.rb
+++ config/recipes/repo/init.rb
@@ -1,15 +1,22 @@
-name_repo = @name_repo; repository = @repository; path_destination = @path_destination
+name_repo = @name_repo; repository = @repository; monorepo = @monorepo; path_destination = @path_destination
 
 ruby_block "repo_#{name_repo}_request" do
   only_if { Logs.info?("[#{repository} (#{name_repo})] create repository") }
   block do
     uri="#{node['git']['api']['endpoint']}/admin/users/#{node['git']['org']['main']}/repos"
     response = Utils.request(uri, method: Net::HTTP::Post, headers: Constants::HEADER_JSON,
-                             user: Env.get(self, 'login'), pass: Env.get(self, 'password'),
-                             body: { name: name_repo, private: false, auto_init: false, default_branch: 'main' }.json )
+      user: Env.get(self, 'login'), pass: Env.get(self, 'password'),
+      body: { name: name_repo, private: false, auto_init: false, default_branch: 'main' }.json )
     Logs.request!(uri, response, [201], msg: "Create repository '#{name_repo}'")
     response.json
   end
+end
+
+ruby_block "dump_variables_#{cookbook_name}" do
+  block do
+    Env.dump(self, ['ip', 'git', 'runner'], repo: cookbook_name)
+  end
+  only_if { monorepo }
 end
 
 execute "repo_#{name_repo}_git_init" do

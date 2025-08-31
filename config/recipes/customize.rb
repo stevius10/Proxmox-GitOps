3ab2fceb8b19973652aa7022a83a550ba5daadfd
Steven <106664555+stevius10@users.noreply.github.com>
2025-08-31 19:10:44 +0200

    feat/assets (#111)

--- config/recipes/customize.rb
+++ config/recipes/customize.rb
@@ -1,1 +1,35 @@
-Chef::Log.info("")
+# Assets
+
+directory (dir_css = ::File.join(node['git']['dir']['custom'], 'public', 'assets', 'css')) do
+  recursive true
+  owner node['app']['user']
+  group node['app']['group']
+  mode '0755'
+end
+
+%w[gitea hide logo].each do |template|
+  template "#{dir_css}/#{template}.css" do
+    source "custom/css/#{template}.css.erb"
+    owner node['app']['user']
+    group node['app']['group']
+    mode '0644'
+  end
+end
+
+# Templates
+
+directory (dir_templates = ::File.join(node['git']['dir']['custom'], 'templates', 'custom')) do
+  recursive true
+  owner node['app']['user']
+  group node['app']['group']
+  mode '0755'
+end
+
+%w[header extra_links extra_tabs body_inner_pre body_inner_post body_outer_pre body_outer_post footer].each do |template|
+  template "#{dir_templates}/#{template}.tmpl" do
+    source "custom/templates/#{template}.tmpl.erb"
+    owner node['app']['user']
+    group node['app']['group']
+    mode '0644'
+  end
+end

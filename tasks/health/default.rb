e8b9a36eba9fc0419fecb2cff39162094535a921
stevius10 <steven.johann.koch@googlemail.com>
2025-08-31 18:53:37 +0200

    use repository description for status

--- tasks/health/default.rb
+++ tasks/health/default.rb
@@ -6,6 +6,9 @@ ctx = { "endpoint"=>ENV["ENDPOINT"], "host"=>ENV["HOST"], "login"=>ENV["LOGIN"],
 containers = Utils.proxmox(ctx, 'nodes/pve/lxc')
 
 containers.each do |container|
+
+  # Get container information
+
   id = container['vmid']
   config = Utils.proxmox(ctx, "nodes/pve/lxc/#{id}/config")
   current = Utils.proxmox(ctx, "nodes/pve/lxc/#{id}/status/current")
@@ -17,12 +20,18 @@ containers.each do |container|
   val = { 'ip'=>ip, 'status'=>status }.compact
   Env.set(ctx, hostname, val, repo: 'health', owner: 'tasks')
 
-  # Set repository state
-  uri = "#{Env.endpoint(ctx)}/repos/main/#{hostname}"
+  # Description
+
+  repository_description = "#{hostname}:#{id} (#{ip}) [#{status}]"
+  repository_url = "https://#{Env.get(self, 'PROXMOX_HOST')}:8006/#v1:0:=lxc%2F#{id}"
+
+  # Set repository description
+
+  uri = "https://#{Env.endpoint(ctx)}/repos/main/#{hostname}"
   Logs.try!("Set #{hostname} to #{val}",[:uri, uri, :hostname, hostname, :status, status], ctx: ctx) do
     response = Utils.request(uri, user: ctx['login'], pass: ctx['password'],
       method: Net::HTTP::Patch, headers: Constants::HEADER_JSON,
-      body: { description: "[#{status}]" }.json)
+      body: { description: repository_description, website: repository_url }.json)
     Logs.request!(uri, response, ctx)
   end
 

fce22430e770a726e6072e1b5678e2925c2cbee0
stevius10 <steven.johann.koch@googlemail.com>
2025-09-09 20:57:05 +0200

    replace custom python built with trixie default 13.5

--- libs/assistant/recipes/default.rb
+++ libs/assistant/recipes/default.rb
@@ -2,17 +2,10 @@ Env.dump(self, ['ip', cookbook_name], repo: cookbook_name)
 
 Common.directories(self, [node['assistant']['dir']['data'], '/app/uv-cache'])
 
-execute 'fix_broken_apt' do
-  command 'apt-get --fix-broken install -y'
-  ignore_failure true
-end
-
-Common.packages(self, %w[mc bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf
-  build-essential libopenjp2-7 libturbojpeg0-dev ffmpeg liblapack3 liblapack-dev
+Common.packages(self, %w[mc bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev
+  build-essential pkg-config libopenjp2-7 libturbojpeg0-dev ffmpeg liblapack3 liblapack-dev
   dbus-broker libpcap-dev libavdevice-dev libavformat-dev libavcodec-dev
-  libavutil-dev libavfilter-dev libmariadb-dev-compat libatlas-base-dev
-  nfs-common wget libncurses5-dev libgdbm-dev libnss3-dev libreadline-dev
-  libsqlite3-dev libbz2-dev python3-venv])
+  libavutil-dev libavfilter-dev libmariadb-dev-compat python3-pip python3-venv python3.13-venv python3.13-dev])
 
 link '/config' do
   to node['assistant']['dir']['data']
@@ -20,40 +13,17 @@ link '/config' do
   group node['app']['group']
 end
 
-Utils.download(self, '/tmp/Python-3.13.5.tgz', url: 'https://www.python.org/ftp/python/3.13.5/Python-3.13.5.tgz')
-
-bash 'install_python3135' do
-  cwd '/tmp'
-  code <<-EOH
-    tar -xzf Python-3.13.5.tgz; cd Python-3.13.5
-    ./configure --enable-optimizations --prefix=/usr/local
-    make -j$(nproc); make altinstall
-  EOH
-  not_if { ::File.exist?('/usr/local/bin/python3.13') }
-end
-
 execute 'install_uv' do
-  command '/usr/local/bin/python3.13 -m pip install uv --break-system-packages'
-  not_if '/usr/local/bin/python3.13 -m pip show uv'
+  command '/usr/bin/python3.13 -m pip install uv --break-system-packages'
+  not_if '/usr/bin/python3.13 -m pip show uv'
 end
 
 execute 'create_environment' do
-  command "uv venv --python=/usr/local/bin/python3.13 #{node['assistant']['dir']['env']}"
+  command "uv venv --python=/usr/bin/python3.13 #{node['assistant']['dir']['env']}"
   user node['app']['user']
   group node['app']['group']
-  environment(
-    # 'HOME' => "/home/#{node['app']['user']}",
-    'UV_CACHE_DIR' => '/app/uv-cache'
-  )
+  environment 'UV_CACHE_DIR' => '/app/uv-cache'
   not_if { ::File.exist?("#{node['assistant']['dir']['env']}/bin/activate") }
-end
-
-execute 'install_environment_pip' do
-  command "#{node['assistant']['dir']['env']}/bin/python -m ensurepip"
-  user node['app']['user']
-  group node['app']['group']
-  # environment('HOME' => "/home/#{node['app']['user']}")
-  not_if { ::File.exist?("#{node['assistant']['dir']['env']}/bin/pip") }
 end
 
 execute 'install_environment_assistant' do
@@ -73,16 +43,18 @@ execute 'set_josepy' do
 end
 
 execute 'create_configurator' do
-  command "python3 -m venv #{node['configurator']['dir']}"
+  command "uv venv --python=/usr/bin/python3.13 #{node['configurator']['dir']}"
   user node['app']['user']
   group node['app']['group']
+  environment 'UV_CACHE_DIR' => '/app/uv-cache'
   not_if { ::File.exist?("#{node['configurator']['dir']}/bin/activate") }
 end
 
 execute 'install_configurator' do
-  command "#{node['configurator']['dir']}/bin/python -m pip install --upgrade pip && #{node['configurator']['dir']}/bin/python -m pip install hass-configurator"
+  command "uv pip install --python #{node['configurator']['dir']}/bin/python hass-configurator"
   user node['app']['user']
   group node['app']['group']
+  environment 'UV_CACHE_DIR' => '/app/uv-cache'
   not_if { ::File.exist?("#{node['configurator']['dir']}/bin/hass-configurator") }
 end
 

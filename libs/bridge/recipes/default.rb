fc9a64a72356315deb5cd85555642901154b32f1
stevius10 <s@gitops.pm>
2025-10-18 07:50:19 +0200

    adopt install signature changes

--- libs/bridge/recipes/default.rb
+++ libs/bridge/recipes/default.rb
@@ -17,7 +17,7 @@ group 'dialout' do
   append true
 end
 
-if (latest_version = Utils.install(self, "Koenkk", "zigbee2mqtt", node['bridge']['dir'], node['bridge']['data']))
+if (latest_version = Utils.install(self, owner: "Koenkk", repo: "zigbee2mqtt", app_dir: node['bridge']['dir']))
 
   if ::File.exist?("/etc/systemd/system/zigbee2mqtt.service")
     execute 'stop_zigbee2mqtt' do

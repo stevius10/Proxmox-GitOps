f786b82c47b878ea91de43047103be1c2a8efa42
stevius10 <s@gitops.pm>
2025-10-19 07:16:46 +0200

    fix the missed self reference

--- config/libraries/default.rb
+++ config/libraries/default.rb
@@ -17,7 +17,7 @@ module Default
 
   def self.snapshot_dir(ctx, default: nil)
     node = Ctx.node(ctx)
-    @snapshot_dir ||= (default.presence ? '/share/snapshots' : presence_or(Env.get(node, "app_snapshot_dir"), config(node, default: true))).to_s
+    @snapshot_dir ||= (default.presence ? '/share/snapshots' : presence_or(Env.get(node, "app_snapshot_dir"), snapshot_dir(node, default: true))).to_s
   end
 
   def self.presence_or(var, default)

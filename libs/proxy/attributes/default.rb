db4d42a376851a114cfb37dd3b1dc5199c002180
stevius10 <s@gitops.pm>
2025-10-19 14:04:18 +0200

    add log roll configuration

--- libs/proxy/attributes/default.rb
+++ libs/proxy/attributes/default.rb
@@ -4,7 +4,11 @@ default['app']['user']                = Default.user(node)
 default['app']['group']               = Default.group(node)
 
 default['proxy']['dir']['app']        = '/app/proxy'
-default['proxy']['dir']['config']     = '/app/proxy/config'
+default['proxy']['dir']['config']     = '/app/proxy//conf.d'
 default['proxy']['dir']['logs']       = '/app/proxy/logs'
 
 default['proxy']['config']['domain']  = 'lan'
+
+default['proxy']['logs']['roll_size'] = '2MiB'
+default['proxy']['logs']['roll_keep'] = '3'
+default['proxy']['logs']['roll_for']  = '1d'

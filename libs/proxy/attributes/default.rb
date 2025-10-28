7df93313e723c053cfa69feb21bc98fa9f738243
Steven Koch <s@gitops.pm>
2025-10-28 18:12:53 +0100

    feat/proxy-certificate-integration (#204)

--- libs/proxy/attributes/default.rb
+++ libs/proxy/attributes/default.rb
@@ -4,11 +4,13 @@ default['app']['user']                = Default.user(node)
 default['app']['group']               = Default.group(node)
 
 default['proxy']['dir']['app']        = '/app/proxy'
+default['proxy']['dir']['certs']      = '/share/.certs'
+default['proxy']['dir']['caddy']      = "#{node['proxy']['dir']['certs']}/caddy"
 default['proxy']['dir']['config']     = '/app/proxy/conf.d'
 default['proxy']['dir']['logs']       = '/app/proxy/logs'
 
-default['proxy']['config']['domain']  = 'lan'
+default['proxy']['config']['domain']  = 'l'
 
 default['proxy']['logs']['roll_size'] = '2MiB'
-default['proxy']['logs']['roll_keep'] = '3'
-default['proxy']['logs']['roll_for']  = '1d'
+default['proxy']['logs']['roll_keep'] = '7'
+default['proxy']['logs']['roll_for']  = '24h'

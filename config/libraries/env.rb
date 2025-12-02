db59874c893825bf8fb693b8bd1bbfe6e8fbd9d9
Steven Koch <s@gitops.pm>
2025-12-02 18:36:58 +0100

    fix/logs-try-robust-avoidance (#232)

--- config/libraries/env.rb
+++ config/libraries/env.rb
@@ -29,7 +29,7 @@ module Env
 
   def self.set_variable(ctx, key, val, repo: nil, owner: nil, upcase: true)
     key = upcase ? key.to_s.upcase : key.to_s
-    Logs.try!("set variable '#{key}' to #{val.try(:mask) || val}", [:repo, repo, :owner, owner], raise: true) do
+    Logs.try!("set variable '#{key}' to #{val.try(:mask) || val}", [:repo, repo, :owner, owner], fail: true) do
       request(Ctx.node(ctx), key, body: ({ name: key, value: (val.is_a?(String) ? val : val.to_json).to_s }.to_json), repo: repo, owner: owner, expect: true)
     end
   end; class << self; alias_method :set, :set_variable; end
@@ -52,12 +52,12 @@ module Env
   end
 
   def self.dump(ctx, *args, repo: nil, owner: nil)
-    Logs.try!("[env.dump]", [:args, args, :repo, repo, :owner, owner], raise: true) do
+    Logs.try!("[env.dump]", [:args, args, :repo, repo, :owner, owner], fail: true) do
       node = Ctx.node(ctx); rec = nil; result = true
-      get = ->(key) { Logs.try!("[env.dump] get", [:key, key], raise: false) { k = key.is_a?(String) ? key : key.to_s; return (node[key] || node[k] || node[k.to_sym]) } }
-      set = ->(key, value) { return value if value.blank?; Logs.try!("[env.dump] set", [:key, key, :value, value], raise: false) { Env.set_variable(node, key, value, repo: repo, owner: owner); true } }
-      req = ->(key, value) { Logs.try!("[env.dump] require", [:key, key, :value, value], raise: false) { v = value; v = v.call(node) if v.respond_to?(:call); v = node.dig(*v) if v.is_a?(Array); set.(key, v); true } }
-      prc = ->(arg) do; Logs.try!("[env.dump] #{arg.inspect}", [:arg, arg], raise: false) do
+      get = ->(key) { Logs.try!("[env.dump] get", [:key, key], fail: false) { k = key.is_a?(String) ? key : key.to_s; return (node[key] || node[k] || node[k.to_sym]) } }
+      set = ->(key, value) { return value if value.blank?; Logs.try!("[env.dump] set", [:key, key, :value, value], fail: false) { Env.set_variable(node, key, value, repo: repo, owner: owner); true } }
+      req = ->(key, value) { Logs.try!("[env.dump] require", [:key, key, :value, value], fail: false) { v = value; v = v.call(node) if v.respond_to?(:call); v = node.dig(*v) if v.is_a?(Array); set.(key, v); true } }
+      prc = ->(arg) do; Logs.try!("[env.dump] #{arg.inspect}", [:arg, arg], fail: false) do
         case arg
           when Hash; arg.each { |key, value| result &&= req.(key, value) }; true
           when Array; (arg.length == 2 && !arg.first.is_a?(Array)) ? (key, value = arg; result &&= req.(key, value)) : arg.each { |x| result &&= prc.(x) }; true

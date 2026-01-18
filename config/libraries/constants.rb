30f33f0079cf09624ac8acb524bb52b0eba25a88
stevius10 <s@gitops.pm>
2026-01-18 07:30:27 +0100

    remove unneeded line break

--- config/libraries/constants.rb
+++ config/libraries/constants.rb
@@ -16,5 +16,4 @@ module Constants
   URI_GITHUB_LATEST = ->(owner, repo) { "#{URI_GITHUB_BASE}/repos/#{owner}/#{repo}/releases/latest" }
   URI_GITHUB_TAG = ->(owner, repo, tag) { tag.blank? ? nil : "#{URI_GITHUB_BASE}/repos/#{owner}/#{repo}/releases/tags/#{'v' unless tag.start_with?('v')}#{tag}" }
 
-
 end

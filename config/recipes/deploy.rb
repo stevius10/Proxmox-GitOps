fa47aae4d744edb174d6a19698548f07d8066a92
Steven Koch <s@gitops.pm>
2026-02-09 18:42:56 +0100

    fix/config-auto-deploy-wait-handle (#269)

--- config/recipes/deploy.rb
+++ config/recipes/deploy.rb
@@ -2,10 +2,11 @@ node.run_state['login']    ||= Env.get(self, 'login')
 node.run_state['password'] ||= Env.get(self, 'password')
 
 ruby_block 'git_auto_deploy' do block do
+  Utils.wait(Constants::API_PATH_REPOSITORIES)
   begin
-    Clients::Git.new(node['git']['api']['endpoint'], node.run_state['login'], node.run_state['password']).run_task("deploy")
+    Clients::Git.new(node['git']['api']['endpoint'], node.run_state['login'], node.run_state['password'])&.run_task("deploy")
   ensure
     Env.set(self, "AUTO_DEPLOY", false)
   end end
-  only_if { [true, "true"].include?(Env.get(self, node['git']['env']['deploy'])) }
+  only_if { Env.get(self, node['git']['env']['deploy']).condition }
 end

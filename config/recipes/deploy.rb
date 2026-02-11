084d24045ba5a6a5c638e7077cb4588f8cf3bab3
stevius10 <s@gitops.pm>
2026-02-11 09:53:03 +0100

    restructure waiting conditions

--- config/recipes/deploy.rb
+++ config/recipes/deploy.rb
@@ -1,10 +1,12 @@
-node.run_state['login']    ||= Env.get(self, 'login')
-node.run_state['password'] ||= Env.get(self, 'password')
-
 ruby_block 'git_auto_deploy' do block do
-  Utils.wait(Constants::API_PATH_REPOSITORIES)
+
+  node.run_state['login']    ||= Env.get(self, 'login')
+  node.run_state['password'] ||= Env.get(self, 'password')
+
+  Utils.wait(Constants::API_PATH_REPOSITORIES.call(node['git']['api']['endpoint'], node['git']['org']['tasks'], "deploy"))
+
   begin
-    Clients::Git.new(node['git']['api']['endpoint'], node.run_state['login'], node.run_state['password'])&.run_task("deploy")
+    Clients::Git.new(node['git']['api']['endpoint'], node.run_state['login'], node.run_state['password']).run_task("deploy")
   ensure
     Env.set(self, "AUTO_DEPLOY", false)
   end end

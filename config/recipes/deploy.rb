593c609ae4f3160cf925f8b678fc96b602e3f2d9
stevius10 <s@gitops.pm>
2026-02-11 15:17:01 +0100

    robust runtime with condition

--- config/recipes/deploy.rb
+++ config/recipes/deploy.rb
@@ -1,18 +1,15 @@
 ruby_block 'git_auto_deploy' do block do
-
   node.run_state['login']    ||= Env.get(self, 'login')
   node.run_state['password'] ||= Env.get(self, 'password')
 
-  Utils.wait(Constants::API_PATH_REPOSITORIES
-    .call(node['git']['api']['endpoint'], node['git']['org']['tasks'], "checkout", "/info/refs"))
-
-  Utils.wait(Constants::API_PATH_REPOSITORIES
-    .call(node['git']['api']['endpoint'], node['git']['org']['tasks'], "deploy", "/actions/workflows"))
+  [["checkout", "/info/refs"], ["deploy", "/actions/workflows"]].each do |repo, path|
+    Utils.wait(Constants::API_PATH_REPOSITORIES.call(node['git']['api']['endpoint'], node['git']['org']['tasks'], repo, path))
+  end
 
   begin
-    Clients::Git.new(node['git']['api']['endpoint'], node.run_state['login'], node.run_state['password'])
-      .run_task("deploy")
+    Clients::Git.new(node['git']['api']['endpoint'], node.run_state['login'], node.run_state['password']).run_task("deploy")
   ensure
     Env.set(self, "AUTO_DEPLOY", false)
   end end
+  only_if { Env.get(self, node['git']['env']['deploy']).condition }
 end

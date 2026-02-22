7fa958e2c06a1c9ee1bd34b937eb5c35fef3ce87
stevius10 <s@gitops.pm>
2026-02-22 14:17:51 +0100

    make request repositories public

--- config/libraries/clients.rb
+++ config/libraries/clients.rb
@@ -8,6 +8,11 @@ module Clients
       @uri, @username, @password = uri, username, password
     end
 
+    def get_repositories(owner=nil, repo=nil, body: nil, method: Net::HTTP::Get, target: nil)
+      repositories = request(Constants::API_PATH_REPOSITORIES.call(@uri, owner, repo, target), body: body, method: method).json
+      repositories.is_a?(Array) ? repositories : [repositories]
+    end
+
     def auto_merge(owner=nil, repo=nil)
       get_repositories(owner, repo)
         .reject { |r|  [0, "0"].include?(r['open_pr_counter']) }
@@ -22,11 +27,6 @@ module Clients
     end
 
     private
-
-    def get_repositories(owner=nil, repo=nil, body: nil, method: Net::HTTP::Get, target: nil)
-      repositories = request(Constants::API_PATH_REPOSITORIES.call(@uri, owner, repo, target), body: body, method: method).json
-      repositories.is_a?(Array) ? repositories : [repositories]
-    end
 
     def request(uri, method: Net::HTTP::Get, body: nil)
       Utils.request(uri, method: method, body: body, headers: Constants::HEADER_JSON,

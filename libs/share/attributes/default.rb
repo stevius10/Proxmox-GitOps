858f08671f3bd56926fee6918479f6e58000c0e4
Steven Koch <s@gitops.pm>
2026-03-03 17:29:30 +0100

    feat/share-global-workspace (#291)

--- libs/share/attributes/default.rb
+++ libs/share/attributes/default.rb
@@ -1,11 +1,15 @@
 include_attribute 'config::default'
 
-default['share']['user'] =
+default['share']['user']    =
   100000    # (/etc/fstab) uid=..
 
-default['share']['group'] =
+default['share']['group']   =
   'root'    # (/etc/fstab) gid=..
 
-default['share']['mount'] = [
-  '/share'  # (container.local.env) MOUNT="/mnt/..:/share/.."
+default['share']['mount']   = [ # (container.local.env) MOUNT="/mnt/..:/share/.."
+
+  (default['share']['path']  = '/share'),
+
+  (default['share']['workspace'] = "#{node['share']['path']}/workspace")
+
 ]

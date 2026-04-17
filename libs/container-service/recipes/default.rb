3f96d356b3f75045da9ff47a3c10a5f8fc40dfc4
Johannes Nguyen <ncloud1708@gmail.com>
2026-04-17 16:52:27 +0200

    fix(container-service): update workflow/env and package handling

--- libs/container-service/recipes/default.rb
+++ libs/container-service/recipes/default.rb
@@ -1,11 +1,24 @@
-Env.dump(self, ['ip', cookbook_name], repo: cookbook_name)
+package 'docker.io' do
+  action :install
+end
 
-Common.packages(self, node['container_service']['docker']['packages'])
+package 'docker-compose-plugin' do
+  action :install
+  only_if "apt-cache policy docker-compose-plugin | awk '/Candidate:/ {print $2}' | grep -vq '(none)'"
+end
+
+package 'docker-compose' do
+  action :install
+  only_if "apt-cache policy docker-compose | awk '/Candidate:/ {print $2}' | grep -vq '(none)'"
+  not_if "dpkg -s docker-compose-plugin >/dev/null 2>&1"
+end
 
 group 'docker' do
   action :modify
-  members [node['app']['user'], 'config']
+  members [node['app']['user']]
   append true
 end
 
-Common.application(self, 'docker', actions: [:enable, :start], verify_cmd: 'systemctl is-active --quiet docker')
+service 'docker' do
+  action [:enable, :start]
+end

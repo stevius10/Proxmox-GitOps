3e3585033f80e51dd9c3e6788a50cf89da2ce2e9
Steven Koch <s@gitops.pm>
2026-04-24 17:55:41 +0200

    feature/local-run-sync-remote (#307)

--- local/run.sh
+++ local/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-log() { echo "[$1] $2"; };
-err() { log "error" "$1"; exit 1; }
+log() { echo "[$1] $2"; }; err() { log "error" "$1"; exit 1; }
 
 cfg() { local lib="${1:-}"
   [[ -f "$lib/config.local.json" ]] && echo "-j $lib/config.local.json" && return 0
@@ -9,36 +8,32 @@ cfg() { local lib="${1:-}"
 
 pre() { set -eo pipefail
   command -v docker >/dev/null || err "missing docker"
-  command -v md5sum >/dev/null || function md5sum() { md5 -q "$1"; } || err "missing md5";
+  command -v md5sum >/dev/null || function md5sum() { md5 -q "$1"; } || err "`md5` missing";
   if [[ "$(basename "$(pwd)")" == "local" ]]; then cd ..; fi
 }; pre
 
 arg() { while [[ $# -gt 0 ]]; do case "$1" in
-  -h|--help)
-    echo -e "\n./$SCRIPT [OPTIONS] [lib] [-h|--help]"
-    echo -e "  -l, --log-level <level>\n  -p, --port <port>\n  -s, --suffixes <list>"
-    echo -e "  -d, --debug\n  -r, --restart"
-    echo -e "\nExamples:"
-    echo -e "  ./$SCRIPT --debug --restart broker\n  ./$SCRIPT -s \"customize\" -l error\n"
-    exit 0 ;;
-  -l|--log-level)
-    [[ $# -gt 1 ]] && LOG_LEVEL="$2" && shift ;;
-  -p|--port)
-    [[ $# -gt 1 ]] && PORT="$2" && shift ;;
-  -s|--suffixes)
-    [[ $# -gt 1 ]] && SUFFIXES="$2" && shift ;;
-  -d|--debug)   LOG_LEVEL="debug" ;;
-  -r|--restart) RESTART="true" ;;
-  -*) ;;
-  *) [[ -z "$LIB" || "$LIB" == "config" ]] && LIB="$1" ;;
-  esac; shift; done
-}; LIB="config"; LOG_LEVEL="info"; PORT=""; RESTART="false"; SCRIPT="$(basename "$0")"; arg "$@"
 
+  -d|--debug)       LOG_LEVEL="debug" ;; -r|--restart) RESTART="true" ;;
+  -l|--log-level)   [[ $# -gt 1 ]] && LOG_LEVEL="$2" && shift ;;
+  -p|--port)        [[ $# -gt 1 ]] && PORT="$2" && shift ;;
+  -s|--suffixes)    [[ $# -gt 1 ]] && SUFFIXES="$2" && shift ;;
+  -h|--help)        echo -e "\n./$SCRIPT [OPTIONS] [<lib>] [-h|--help]"; echo -e "  -l, --log-level <level>\n  -p, --port <port>\n  -s, --suffixes <list>\n  -d, --debug\n  -r, --restart"
+                    echo -e "\nUsage:\n  ./$SCRIPT --debug --restart broker\n  ./$SCRIPT -s \"customize\" -l error\n  ./$SCRIPT sync\n"; exit 0 ;;
+  sync)  # req. preassigned keys (/local/share/.keys/{ID})
+    source ./container.env || source ../container.env || true ;;
+
+  -*) ;; *) [[ -z "$LIB" || "$LIB" == "config" ]] && LIB="$1" ;; esac; shift; done
+}
+LIB="config"; LOG_LEVEL="info"; PORT=""; RESTART="false"; SCRIPT="$(basename "$0")"; arg "$@";
 
 NAME="$(basename "$(pwd)${LIB:+/config/libs/$LIB}" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]//g')"
 LIB="${LIB:-config}"; LOCAL="./local"
 CONFIG=$(cfg "${LOCAL}" || cfg "./libs/${LIB:-config}" || true)
 LIBS="['/tmp/config','/tmp/config/libs']"
+
+
+# Container
 
 DOCKER_IMAGE="$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]//g')"
 DOCKER_FILE="${LOCAL}/Dockerfile"; DOCKER_WAIT="${DOCKER_WAIT:-3}"
@@ -49,15 +44,8 @@ HASH_SUM=$(echo "$(md5sum "$DOCKER_FILE" | awk '{print $1}')${HASH_BASE}" | md5s
 HASH_FILE="${LOCAL}/.local.hash"
 HASH_STORED=$(cat "$HASH_FILE" 2>/dev/null || true)
 
-case "$(uname -m)" in
-  aarch64|arm64) TARGETARCH="arm64" ;;
-  x86_64) TARGETARCH="amd64" ;;
-  *) TARGETARCH="unknown" ;;
-esac
-export DOCKER_DEFAULT_PLATFORM="${DOCKER_DEFAULT_PLATFORM:-linux/${TARGETARCH}}"
-
-
-log "container" ""
+case "$(uname -m)" in aarch64|arm64) TARGETARCH="arm64" ;; x86_64) TARGETARCH="amd64" ;; *) TARGETARCH="amd64" ;; esac;
+export DOCKER_DEFAULT_PLATFORM="${DOCKER_DEFAULT_PLATFORM:-linux/$TARGETARCH}"
 
 if docker ps -a --format '{{.Names}}' | grep -q "^${DOCKER_CONTAINER}$"; then
     log "container" "clean"
@@ -68,51 +56,54 @@ if docker ps -a --format '{{.Names}}' | grep -q "^${DOCKER_CONTAINER}$"; then
 fi
 
 if [[ -z "$(docker images -q "${DOCKER_IMAGE}")" || "$HASH_STORED" != "$HASH_SUM" ]]; then
-    log "container" "build"
-    docker build --no-cache --build-arg TARGETARCH="$TARGETARCH" -t "$DOCKER_IMAGE" -f "$DOCKER_FILE" "$(pwd)" || err "image build failed"
+    log "container" "image"
+    docker build --no-cache --build-arg TARGETARCH="$TARGETARCH" -t "$DOCKER_IMAGE" -f "$DOCKER_FILE" "$(pwd)" || err "container: build"
     echo "$HASH_SUM" > "$HASH_FILE"
 fi
 
-log "container" "run"
+log "container" "start"
 CONTAINER_ID=$(docker run -d --privileged --cgroupns=host -v /sys/fs/cgroup:/sys/fs/cgroup:rw --add-host=host.docker.internal:host-gateway \
     $( [[ -d "${LOCAL}/share" ]] && echo "-v ${LOCAL}/share:/share:ro " ) \
     $( [[ "$LIB" != "config" ]] && echo "-p ${PORT:-80}:${PORT:-80} -e HOST=host.docker.internal" || echo "-p ${PORT:-8080}:8080 -p 2222:2222" ) \
-    --name "$DOCKER_CONTAINER" --platform "linux/${TARGETARCH}" -w /tmp/config "$DOCKER_IMAGE" sleep infinity) || err "failed to start container"
-log "container" "${DOCKER_CONTAINER} [${CONTAINER_ID:0:6}]"
+    --name "$DOCKER_CONTAINER" --platform "linux/${TARGETARCH}" -w /tmp/config "$DOCKER_IMAGE" sleep infinity) || err "container: startup"
 
-log "configuration" ""
 
-sync() {
-  docker exec "$CONTAINER_ID" bash -c "rm -rf /tmp/config/*" || err "cleanup error"
-  docker cp "$(pwd)/." "$CONTAINER_ID:/tmp/config/" || err "remote error"
+# Main
+
+files() { log "${DOCKER_CONTAINER} [${CONTAINER_ID:0:6}]" "files"
+  docker exec "$CONTAINER_ID" bash -c "rm -rf /tmp/config/*" || err "files: cleanup"
+  docker cp "$(pwd)/." "$CONTAINER_ID:/tmp/config/" || err "files: add"
 
   if [[ "${LIB}" != "config" ]]; then
-    log "configuration" "sync libraries (${LIB})"
     docker exec "$CONTAINER_ID" bash -c "mkdir -p '/tmp/config/libs/${LIB}/libraries' && \
-      cp -a /tmp/config/config/libraries/. '/tmp/config/libs/${LIB}/libraries/'" || err "copy error"
+      cp -a /tmp/config/config/libraries/. '/tmp/config/libs/${LIB}/libraries/'" || err "files: libraries"
   fi
-}
+}; files
 
-configuration() {
-  local recipe="$LIB${1:+::$1}"
-  log "configuration" "sync" && sync
-  log "configuration" "execute $recipe (LIB=$LIB LOG_LEVEL=$LOG_LEVEL RESTART=$RESTART SUFFIXES=$SUFFIXES)"
-  command='sudo $(sudo -u config env) PWD=/tmp/config --preserve-env=ID,HOST \
-    cinc-client -l '"$LOG_LEVEL"' --local-mode --config-option node_path=/tmp/nodes \
-      --config-option cookbook_path='"$LIBS"' '"$CONFIG"' -o '"$recipe"''
-  docker exec "$CONTAINER_ID" bash -c "$command"  || err "execution error"
-  log "configuration" "executed ($recipe)"
-}
+if [[ -n "${IP}" && -n "${ID}" ]]; then # (sync) . container.env
 
-configuration ""
+  # Synchronize Remote
+  docker exec "$CONTAINER_ID" bash -c "tar -cz -C /tmp config | ssh -o StrictHostKeyChecking=no -i \"/share/.keys/${ID}\" \"config@${IP}\" '\
+    sudo rm -rf /tmp/config && sudo tar -xz -C /tmp; sudo -E IP=\"${IP}\" ID=\"${ID}\" PWD=/tmp/config \
+      cinc-client -j \"/tmp/config/local/config\$( [ -f /tmp/config/local/config.local.json ] && echo \".local\" ).json\" --local-mode --config-option cookbook_path=\"/tmp/config\" -o \"config::repo\" '" || err "sync"; exit 0
+else
 
-if [[ -z "${SUFFIXES+x}" ]]; then if [[ "${LIB}" != "config" ]]; then SUFFIXES=("default"); else SUFFIXES=("repo"); fi; fi
-while true; do
-  echo -n "[run] $LIB::${SUFFIXES[*]} or $LIB::"; read -r input; [[ -n "$input" ]] && SUFFIXES=($input)
-  for s in "${SUFFIXES[@]}"; do configuration "$s"; done
+  # Configure Local
+  configuration() { local recipe="${1:-default}"; log "${DOCKER_CONTAINER} [${CONTAINER_ID:0:6}]" "$LIB::$recipe"
+    docker exec "$CONTAINER_ID" bash -c 'sudo $(sudo -u config env) PWD=/tmp/config --preserve-env=ID,HOST \
+      cinc-client -l '"$LOG_LEVEL"' --local-mode --config-option node_path=/tmp/nodes --config-option cookbook_path='"$LIBS"' '"$CONFIG"' -o '"$LIB::$recipe"''  || err "configuration: execution"
+  }; configuration
 
-  if [[ "${RESTART}" == "true" ]]; then
-    log "configuration" "restart [${CONTAINER_ID:0:6}]"
-    sleep "$DOCKER_WAIT" && docker restart $CONTAINER_ID
-  fi
-done
+  # Local Development and Reconfiguration
+  if [[ -z "${SUFFIXES+x}" ]]; then if [[ "${LIB}" != "config" ]]; then SUFFIXES=("default"); else SUFFIXES=("repo"); fi; fi
+
+  while true; do echo -n "$LIB::${SUFFIXES[*]} or $LIB::"; read -r input; [[ -n "$input" ]] && SUFFIXES=($input)
+    files && (for s in "${SUFFIXES[@]}"; do configuration "$s"; done)
+
+    if [[ "${RESTART}" == "true" ]]; then
+      log "${DOCKER_CONTAINER} [${CONTAINER_ID:0:6}]" "restart"
+      sleep "$DOCKER_WAIT" && docker restart $CONTAINER_ID
+    fi
+  done
+
+fi

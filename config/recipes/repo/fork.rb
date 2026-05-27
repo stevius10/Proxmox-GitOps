a143eea507cff46cd98f74daf71750c8df3224ce
Steven Koch <s@gitops.pm>
2026-05-27 14:27:49 +0200

    chore/tasks-repository-state (#330)

--- config/recipes/repo/fork.rb
+++ config/recipes/repo/fork.rb
@@ -11,7 +11,8 @@ if @monorepo or @repository.include?('libs/')
     req.call(fork, method: Net::HTTP::Delete) if req.call(fork, expect: true, raise: false)
 
     req.call("#{main}/forks", method: Net::HTTP::Post, body: { organization: node['git']['org']['stage'] })
-    req.call(fork, method: Net::HTTP::Patch, body: { has_actions: true, has_issues: false, has_wiki: false, has_projects: false, has_packages: false, has_releases: false })
+    req.call(fork, method: Net::HTTP::Patch, body: { description: "", website: "",
+      has_actions: true, has_issues: false, has_wiki: false, has_projects: false, has_packages: false, has_releases: false })
 
     req.call("#{fork}/pulls", method: Net::HTTP::Post, body: { title: "Staging Pull Request", body: "Created automatically for deployment.",
       head: "main", base: "release" } ) unless @is_bootstrap

3e830e6d0bd4496ee68cd1b6d47512372dc562a5
Steven Koch <s@gitops.pm>
2026-07-18 12:41:37 +0200

    feat/shift-gitea-runner (#335)

--- config/recipes/runner.rb
+++ config/recipes/runner.rb
@@ -2,7 +2,7 @@ ruby_block 'runner' do block do
   Common.directories(self, node['runner']['dir']['app'])
 
   Utils.download(node, "#{node['runner']['dir']['app']}/#{self.recipe_name}",
-    "#{node['runner']['app']['mirror']}#{node['runner']['app']['version']}/act_runner-#{node['runner']['app']['version']}-linux-#{Utils.arch()}")
+    "#{node['runner']['app']['mirror']}#{node['runner']['app']['version']}/gitea-runner-#{node['runner']['app']['version']}-linux-#{Utils.arch()}")
 
   Utils.wait("#{node['git']['host']['local']}:#{node['git']['port']['http']}")
 
